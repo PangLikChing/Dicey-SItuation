301f60e21bc697d41f80d65b3a5154c4505fc4c5
Smet19 <smet1990@gmail.com>
2022-07-15 20:13:41 -0400

    Redid player prototype, scaled up the map

--- Assets/Scripts/Characters/Player/PlayerLogic.cs
+++ Assets/Scripts/Characters/Player/PlayerLogic.cs
@@ -6,13 +6,6 @@ public class PlayerLogic : CharacterBase
 {
     public float moveSpeed = 10.0f;
 
-    [SerializeField] protected DiceController dice;
-
-    protected virtual void Awake()
-    {
-        if (dice == null) dice = GetComponentInChildren<DiceController>();
-    }
-
     protected virtual void Update()
     {
         #region Movement
@@ -21,27 +14,10 @@ public class PlayerLogic : CharacterBase
 
         Vector3 movDir = new Vector3(horizontal, 0, vertical);
 
-        if(movDir != Vector3.zero)
+        if (movDir != Vector3.zero)
         {
             transform.Translate(movDir * moveSpeed * Time.deltaTime, Space.World);
         }
-        #endregion
-        
-        #region RotateToMousePos
-        Vector3 mousePos = Input.mousePosition;
-
-        Ray ray = Camera.main.ScreenPointToRay(mousePos);
-
-        Vector3 lookAt = Vector3.zero;
-        
-        if (Physics.Raycast(ray, out RaycastHit hit))
-        {
-            lookAt = hit.point;
-        }
-
-        lookAt.y = transform.position.y;
-
-        transform.LookAt(lookAt);
         #endregion
     }
 }
